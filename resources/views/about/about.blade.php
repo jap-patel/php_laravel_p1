60e72e5aa765dcfbb7208c2bd76228ae6a818fff
jhirpara <jhirpara@argusoft.com>
2026-01-29 18:52:55 +0530

    pushing initial laravel code

---
+++ resources/views/about/about.blade.php
@@ -0,0 +1,18 @@
+<!-- <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about route</title>
+</head>
+<body>
+    <div>
+        <h1>About Page</h1>
+        <h2>{{$name}}</h2>
+    </div>
+</body>
+</html> -->
+
+<x-layout>
+    <h1>About Page</h1>
+        <h2>{{$name}}</h2>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>shunya</title>
+    @vite(['resouces/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-grey-900 text-white">
+    <div>
+        <a href="/about">about page</a>
+        <h1>hello world</h1>
+    </div>
+</body>
+</html>
